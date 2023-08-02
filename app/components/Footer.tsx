d7efc5779c9788ae4738a919be85b7aa55c1c981
saisabari24 <sai77sab@gmail.com>
2023-08-02 22:40:37 +0530

    dev sab commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flexStart footer">
-      <div className="flex flex-col gap-12 w-full">Footer</div>
+      <div className="flex flex-col gap-14 w-full">Footer</div>
     </footer>
   );
 };
